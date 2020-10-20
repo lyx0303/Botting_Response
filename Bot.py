0b7021499a7e00056a41f674adc220e9efbd1fa6
lyx0303 <69945333+lyx0303@users.noreply.github.com>
2020-10-20 14:45:44 +0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,7 +13,7 @@ while True:
     
     if each_word == "sad":
       feelings_list.append("sad")
-      encouragement_list.append("I hope the day treats you better!")
+      encouragement_list.append("interesting")
       counter += 1
     if each_word == "happy":
       feelings_list.append("happy")
